d111a57cdb623c790fa0decd51df13610fad0193
ssomb4 <80516735+ssomb4@users.noreply.github.com>
2021-04-16 16:07:55 +0100

    Página Inicial

--- js/source.js
+++ js/source.js
@@ -1,3 +1,14 @@
+let truncateString = function(str, num)
+{
+    if (str.length > num) {
+        return str.slice(0, num) + "...";
+      } else {
+        return str;
+      } 
+}
+
+writeOutput(truncateString("Hoje está um lindo dia", 8));
+
 let titleCase = function(str)
 {
     return str.replace(
